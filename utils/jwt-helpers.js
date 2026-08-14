8847aad03e6c32d9d3248cf5730a057a3ee431c4
ankur <ankur188188@gmail.com>
2026-08-14 22:45:39 +0530

    added access token  and refresh token

--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -2,8 +2,8 @@ import jwt from 'jsonwebtoken';
 
 function jwtTokens({user_id, user_name, user_email}) {
     const user  = {user_id, user_name, user_email};
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2m'});
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '30m'});
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'});
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '120m'});
     return ({accessToken, refreshToken});
 }
 
